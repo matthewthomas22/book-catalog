d1532f62218a5faf28b5f01e34e4d985fd570083
Matthew Thomas <matthewthomas@Matthews-MacBook-Air.local>
2026-05-04 23:14:23 +0700

    added migration for postgresql database in mac os

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Book extends Model
 {
-    //
+    public function author(){
+        return $this->belongsTo(Author::class);
+    }
+
+    public function publisher(){
+        return $this->belongsTo(Publisher::class);
+    }
 }
---
+++ database/migrations/2026_05_04_140902_create_books_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            // One author have many books
+            $table->foreignId('author_id')->constrained()->cascadeOnDelete();
+            // One Book have one publisher
+            $table->foreignId('publisher_id')->constrained()->onDelete('cascade');
+            $table->date('published_date')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('books');
+    }
+};
